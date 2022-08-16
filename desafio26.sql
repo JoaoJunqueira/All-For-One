25ac7becd7c7b867306253dc13628ba0092ec246
JoaoJunqueira <vitjunqueira38@gmail.com>
2022-08-16 18:30:18 -0300

    requisito 26: conserto valor unit_price

--- desafio26.sql
+++ desafio26.sql
@@ -1,4 +1,2 @@
 DELETE FROM northwind.order_details
-WHERE unit_price > 10000;
-
-# https://stackoverflow.com/questions/11448068/mysql-error-code-1175-during-update-in-mysql-workbench
+WHERE unit_price > 10;
